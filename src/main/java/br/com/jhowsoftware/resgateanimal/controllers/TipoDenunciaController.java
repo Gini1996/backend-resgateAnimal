00160e52bb8430780683251388ec8ef343ba88b7
Leonardo Ferreira <leonardo.gini.96@hotmail.com>
2024-02-28 17:16:09 -0300

    Implementação GetFindAll

--- src/main/java/br/com/jhowsoftware/resgateanimal/controllers/TipoDenunciaController.java
+++ src/main/java/br/com/jhowsoftware/resgateanimal/controllers/TipoDenunciaController.java
@@ -1,8 +1,11 @@
 package br.com.jhowsoftware.resgateanimal.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +20,12 @@ public class TipoDenunciaController
 {
 	@Autowired
 	private TipoDenunciaService tipoDenunciaService;
+	
+	@GetMapping
+	public List<TipoDenunciaDTO> findAll()
+	{
+		return tipoDenunciaService.findAll();
+	}
 	
 	@PostMapping("/addTpDenuncia")
 	public ResponseEntity<String> addTpDenuncia(@RequestBody TipoDenunciaDTO body)
